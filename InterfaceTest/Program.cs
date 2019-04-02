37f2a09b830b0f6c12509b86a0d4fa3bf43c49d1
stupid999 <aierzhiguang2@gmail.com>
2019-04-02 19:48:56 +0800

    學習Interface用法

---
+++ InterfaceTest/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceTest
+{
+    interface IType
+    {
+        string Type {  get; }
+        int Atk { get;  }
+        int Def { get;  }
+        int Hp { get;  }
+        int isDamaged(int hp,int atk, int def);    
+    }
+
+    class CKnight:IType
+    {
+        string type="knight";
+        public string Type { get; }
+        int atk = 2;
+        public int Atk { get; }
+        int def = 10;
+        public int Def { get; }
+        int hp = 100;
+        public int Hp { get; }
+        public int isDamaged(int hp,int atk,int def)
+        {
+            int damage = atk - def ;
+            if (damage <= 0)
+                damage = 1;
+            hp -= damage;
+            if (hp <= 0)
+            {
+                hp = 0;
+                Console.Write("Died");
+            }
+            return hp;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
